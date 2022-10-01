e09441fb1ab3518fc072b84d1bffa12d1b2d6459
SdmfDovi <dovi.yarmush@gmail.com>
2022-10-01 21:47:39 +0300

    Added Apo connectivity for coin values.

--- src/main/java/Coins/ILS.java
+++ src/main/java/Coins/ILS.java
@@ -1,14 +1,45 @@
 package Coins;
 
-public class ILS extends Coin {
-    final double value =0.28;
-   public double getValue(){
-       return value;
-   }
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-    @Override
-    public double calculate(double input){
-        return input / getValue();
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ILS extends Coin {
+    final double value = apiValue();
+
+    public ILS() throws IOException {
     }
 
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public double calculate(double input) {
+        return input * getValue();
+    }
+
+    public double apiValue() throws IOException {
+        String url_str = "https://api.exchangerate.host/convert?from=USD&to=ils";
+
+        URL url = new URL(url_str);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject jsonobj = root.getAsJsonObject();
+
+        String req_result = jsonobj.get("result").getAsString();
+
+        double currentValue= Double.parseDouble(req_result);
+        return currentValue;
+    }
 }
--- src/main/java/Coins/USD.java
+++ src/main/java/Coins/USD.java
@@ -1,11 +1,41 @@
 package Coins;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class USD extends Coin{
-    final double value =3.52;
+    final double value =apiValue();
+
+    public USD() throws IOException {
+    }
+
     public double getValue(){
         return value;
     }
     public double calculate(double input){
-        return input / getValue();
+        return input * getValue();
+    }
+    public double apiValue() throws IOException {
+        String url_str = "https://api.exchangerate.host/convert?from=ils&to=usd";
+
+        URL url = new URL(url_str);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject jsonobj = root.getAsJsonObject();
+
+        String req_result = jsonobj.get("result").getAsString();
+
+        double currentValue = Double.parseDouble(req_result);
+        return currentValue;
     }
 }
